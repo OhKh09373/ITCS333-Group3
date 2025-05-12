6bf334246f907cc9dc58df4dbb46180ed5c33e19
OhKh09373 <202209373@stu.uob.edu.bh>
2025-05-12 13:29:25 +0300

    Changes

--- Marketplace/Script.js
+++ Marketplace/Script.js
@@ -1,1 +1,10 @@
-let myitem = document.querySelector(".action-button");
+let myitem = document.querySelector(".action-button#login");
+let user = document.getElementById("login-section");
+let mylist = document.getElementById("listings-section");
+
+myitem.addEventListener("click", login);
+
+function login() {
+  user.style.display = "none";
+  mylist.style.display = "flex";
+}
